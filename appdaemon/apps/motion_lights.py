f8cc1798884e446786d99b775539e8c981ec7e2a
superminor2 <super@superminor2.net>
2022-01-07 10:40:51 -0600

    Update 2021-01-07
    Fixed timer handle not clearing, cleaned up logging

--- appdaemon/apps/motion_lights.py
+++ appdaemon/apps/motion_lights.py
@@ -1,6 +1,8 @@
 import hassapi as hass
 import helpers
 
+version = "2022-01-07"
+
 # App to Automatically control lights based on motion or other activity
 #
 # Args:
@@ -27,8 +29,6 @@ import helpers
 
 class MotionLights(hass.Hass):
     def initialize(self):
-        self.log("Beginning initialization")
-
         # Create a variable to store the timer handle
         self.handle = None 
 
@@ -51,12 +51,12 @@ class MotionLights(hass.Hass):
         if "entity_on" in self.args:
             self.entity_on = self.args["entity_on"]
         else:
-            self.log("Error, no entity on specfied")
+            self.error("No entity on specfied")
 
         if "entity_off" in self.args:
             self.entity_off = self.args["entity_off"]
         else:
-            self.log("No entity off specfied, using entity_on ({0})".format(self.entity_on))
+            #self.log("No entity off specfied, using entity_on ({0})".format(self.entity_on))
             self.entity_off = self.entity_on
 
         # Check if sensor entity or sensor entity list is defined
@@ -65,30 +65,52 @@ class MotionLights(hass.Hass):
             if type(sensor) == str:
                 # Single entity
                 self.listen_state(self.motion_callback, self.args["sensor"])
-                self.log("Subscribing to sensor {0}".format(sensor))
+                #self.log("Subscribing to sensor {0}".format(sensor))
             elif type(sensor) == list:
-                self.log("Subscribing to sensors {0}".format(sensor))
+                #self.log("Subscribing to sensors {0}".format(sensor))
                 for entity in sensor:
                     self.listen_state(self.motion_callback, entity)
         else:
-            self.log("Error, No sensor specified. Please edit your app configuration")
+            self.error("No sensor specified. Please edit your app configuration")
 
         # Check for condition entity, subscribe and set variable
         if "condition" in self.args:
             self.condition_entity = self.args["condition"]
             self.listen_state(self.condition_callback, self.condition_entity)
-            self.log("Subscribed to condition {0}".format(self.condition_entity))
+            #self.log("Subscribed to condition {0}".format(self.condition_entity))
         else:
             self.condition_entity = False
         
+        # Clean up light current state 
+        # This helps fix lights after a restart
         if self.get_state(self.entity_on) == "on":
             if self.condition_entity:
                 if self.get_state(self.condition_entity) == "off":
                     self.log("The light is currently on, but the condition doesn't allow it to be on. Turning off")
                     self.light_off()
+                else:
+                    if self.brightness_off:
+                        # The light and condition are on, check if it is at brightness_off and restart timer if not
+                        current_brightness = self.get_state(self.entity_on, attribute="brightness")
+                        # Check if the numbers are within 2 of each other (to avoid conversion issues)
+                        if (abs(current_brightness - self.brightness_off) <= 2):
+                            self.log("Detected light is already at the brightness_off value, leaving alone")
+                        else:
+                            self.log("Restarted timer because the light is at {0} and brightness_off is {1}".format(current_brightness, self.brightness_off))
+                            self.restart_timer()
+                    else:
+                        #The light doesn't have a brightness_off set, restart the timer since it is on
+                        self.log("Restarted timer because the light is currently on")
+                        self.restart_timer()
+            else:
+                # The light is on, and there is no condition. It could be on from a previous activation. Restart the timer
+                # This gives the event time to happen again to keep the light on
+                self.log("Detected light is on, Restarting timer")
+                self.restart_timer()    
 
-        self.restart_timer()
-        self.log("Initialization Complete")
+    def terminate(self):
+        if self.handle != None:
+            self.cancel_timer(self.handle)
 
     def motion_callback(self, entity, attribute, old, new, kwargs):
         if self.condition_entity:
@@ -96,28 +118,35 @@ class MotionLights(hass.Hass):
         else:
             automation_allowed = "on"
 
-        if new == "on" and automation_allowed == "on":
+        if (old == "off" and new == "on") and automation_allowed == "on":
+            self.log("Triggered by state of {0}".format(entity))
             self.light_on()
 
     def condition_callback(self, entity, attribute, old, new, kwargs):
         # If the condition entity just switched on, and there is a brightness_off defined, turn on the light to the "off" brightness
-        if new == "on" and self.brightness_off:
+        if (old == "off" and new == "on") and self.brightness_off:
             if self.get_state(self.entity_on) == "off":
                 self.turn_on(self.entity_on, brightness=self.brightness_off)
-                self.log("Turned on {0} to brightness_off because condition just turned on".format(self.entity_on)) 
+                self.log("Turned on {0} to brightness_off {1} because condition just turned on".format(self.entity_on, self.brightness_off)) 
 
         # If the off modifier entity just switched off, and the light is on *at the brightness_off* level turn off the light
         # *add me
-        if new == "off":
+        if (old == "on" and new == "off"):
             current_state = self.get_state(self.entity_on)
             current_brightness = self.get_state(self.entity_on, attribute="brightness")
-            if (current_state == "on") and (current_brightness == self.brightness_off):
-                self.log("Turned off {0} because condition just turned off and the light was at the brightness_off value".format(self.entity_on))
-                self.turn_off(self.entity_off) 
+            if (current_state == "on"):
+                # Check if the numbers are within 2 of each other (to avoid conversion issues)
+                if (abs(current_brightness - self.brightness_off) <= 2):
+                    self.log("Turned off because condition just turned off while at the brightness_off value".format(self.entity_on))
+                    self.turn_off(self.entity_off) 
+                else:
+                    self.log("Restarted timer because condition turned off, while the light is on ({0}) (But not at the brightness_off value {1}".format(current_brightness, self.brightness_off))
+                    self.restart_timer()
+            
 
-    def light_off_callback(self, kwargs):
+    def timer_callback(self, kwargs):
         # Receives timer events
-        self.log("Timer Finished")
+        self.handle = None 
         self.light_off()            
             
     def light_on(self):
@@ -125,10 +154,10 @@ class MotionLights(hass.Hass):
         # This section won't run unless the condition allows for it
        
         if self.brightness_on:
-            self.log("Event Triggered: turning {0} on to brightness {1} and restarting timer".format(self.entity_on, self.brightness_on))
+            self.log("Turning {0} on to brightness {1} and restarting timer".format(self.entity_on, self.brightness_on))
             self.turn_on(self.entity_on, brightness=self.brightness_on)
         else:
-            self.log("Event Triggered: turning {0} on and restarting timer".format(self.entity_on))
+            self.log("Turning {0} on and restarting timer".format(self.entity_on))
             self.turn_on(self.entity_on)
        
         # Restart off timers
@@ -159,8 +188,6 @@ class MotionLights(hass.Hass):
 
     def restart_timer(self):
         if self.handle != None:
-            if self.info_timer(self.handle) != None:
-                self.log("Cancelling Timer")
-                self.cancel_timer(self.handle)
-        self.log("Starting Timer")
-        self.handle = self.run_in(self.light_off_callback, self.delay)
+            self.cancel_timer(self.handle)
+        self.handle = self.run_in(self.timer_callback, self.delay)
+        
